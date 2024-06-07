651d2daa96e89194634baadab70ed9101a53a34d
miguelespp <carlos.espinoza24@unmsm.edu.pe>
2024-06-07 00:41:37 -0500

    feat: fix pages

--- Frontend-RouterCraft/src/pages/Login.tsx
+++ Frontend-RouterCraft/src/pages/Login.tsx
@@ -11,14 +11,20 @@ const Login = () => {
   };
 
   const onSubmit = async (values: typeof initialValues) => {
+    // let response;
     try {
-      const response = await ApiInstance.post("/auth/login", values)
-      console.log(response);
-    } catch (error:any) {
-      if(error.response){
-        console.log(error.response.data)
+      const response = await ApiInstance.post("/auth/login", values);
+      console.log(response.data);
+    } catch (error: any) {
+      if (error.response) {
+        const response = error.response.data;
+        console.log(response)
       }
     }
+
+    // response.success
+    //   ? console.log("Login success")
+    //   : alert("Error al iniciar sesion");
   };
 
   const validationSchema = Yup.object({
@@ -31,47 +37,48 @@ const Login = () => {
   });
 
   return (
-    <section className="bg-slate-500 w-full h-full min-h-screen">
-      <div className="container m-auto py-[10%] max-w-[40%]">
-        <div className="grid grid-cols-1 h-full md:grid-cols-2 gap-4">
-          <div className="flex items-center justify-center">
-            <div className="  max-w-md w-full space-y-8">
-              <div>
-                <h2 className="text-3xl font-bold text-center text-white">
-                  Login
-                </h2>
-              </div>
-              <Formik
-                initialValues={initialValues}
-                onSubmit={onSubmit}
-                validationSchema={validationSchema}
-              >
-                {({ values, errors, handleChange, handleSubmit }) => (
-                  <form onSubmit={handleSubmit} className="space-y-4">
-                    <Input
-                      label="Correo"
-                      type="email"
-                      name="email"
-                      placeholder="example@gmail.com"
-                      error={errors.email}
-                      onChange={handleChange}
-                      value={values.email}
-                    />
-                    <Input
-                      label="Contraseña"
-                      type="password"
-                      name="password"
-                      placeholder="**************"
-                      error={errors.password}
-                      onChange={handleChange}
-                      value={values.password}
-                    />
-
-                    <Button type="submit" value="Registrar" />
-                  </form>
-                )}
-              </Formik>
+    <section className="container bg-slate-500 h-full min-h-screen grid grid-cols-1 sm:grid-cols-2 content-center">
+      <div className="w-full h-full">
+        <img className="" src="src/assets/login.jpg" alt="login" />
+      </div>
+      <div className="w-full mx-auto my-auto py-[15%] max-w-[60%]">
+        <div className="grid grid-cols-1 gap-6 content-center h-full p-5 ">
+          <div className="max-w-md w-full space-y-8 m-auto">
+            <div className="py-2 my-4">
+              <h2 className="text-3xl font-bold text-center text-white">
+                Login
+              </h2>
             </div>
+            <Formik
+              initialValues={initialValues}
+              onSubmit={onSubmit}
+              validationSchema={validationSchema}
+            >
+              {({ values, errors, handleChange, handleSubmit }) => (
+                <form onSubmit={handleSubmit} className="space-y-6 py-4">
+                  <Input
+                    label="Correo"
+                    type="email"
+                    name="email"
+                    placeholder="example@gmail.com"
+                    error={errors.email}
+                    onChange={handleChange}
+                    value={values.email}
+                  />
+                  <Input
+                    label="Contraseña"
+                    type="password"
+                    name="password"
+                    placeholder="**************"
+                    error={errors.password}
+                    onChange={handleChange}
+                    value={values.password}
+                  />
+
+                  <Button type="submit" value="Registrar" />
+                </form>
+              )}
+            </Formik>
           </div>
         </div>
       </div>
--- Frontend-RouterCraft/src/pages/Register.tsx
+++ Frontend-RouterCraft/src/pages/Register.tsx
@@ -14,11 +14,11 @@ const Register = () => {
 
   const onSubmit = async (values: typeof initialValues) => {
     try {
-      const response = await ApiInstance.post("/auth/register", values)
-      console.log(response);
-    } catch (error:any) {
-      if(error.response){
-        console.log(error.response.data)
+      const response = await ApiInstance.post("/auth/register", values);
+      console.log(response.data);
+    } catch (error: any) {
+      if (error.response) {
+        console.log(error.response.data);
       }
     }
   };
@@ -39,72 +39,70 @@ const Register = () => {
   });
 
   return (
-    <section className="min-h-screen " style={{backgroundImage: `url('../assets/login.jpg')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
-      <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-[5%] lg:px-8 bg-sky-500 ">
+    <section className="container min-h-screen">
+      <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-[5%] lg:px-8 bg-blue-800">
         <div className="rounded-lg h-full w-full">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Logo"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Register
-          </h2>
-        </div>
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              className="mx-auto h-16 w-auto rounded-full"
+              src="/logo.jpeg"
+              alt="Logo"
+            />
+            <h2 className="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Register
+            </h2>
+          </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <Formik
-                initialValues={initialValues}
-                onSubmit={onSubmit}
-                validationSchema={validationSchema}
-              >
-                {({ values, errors, handleChange, handleSubmit }) => (
-                  <form onSubmit={handleSubmit} className="space-y-6">
-                    <Input
-                      label="Nombre"
-                      type="text"
-                      name="name"
-                      placeholder="Ingrese su nomre"
-                      error={errors.name}
-                      onChange={handleChange}
-                      value={values.name}
-                    />
-                    <Input
-                      label="Correo"
-                      type="email"
-                      name="email"
-                      placeholder="example@gmail.com"
-                      error={errors.email}
-                      onChange={handleChange}
-                      value={values.email}
-                    />
-                    <Input
-                      label="Contraseña"
-                      type="password"
-                      name="password"
-                      placeholder="**************"
-                      error={errors.password}
-                      onChange={handleChange}
-                      value={values.password}
-                    />
-                    <Input
-                      label="Confirmar contraseña"
-                      type="password"
-                      name="password_confirmation"
-                      placeholder="**************"
-                      error={errors.password_confirmation}
-                      onChange={handleChange}
-                      value={values.password_confirmation}
-                    />
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <Formik
+              initialValues={initialValues}
+              onSubmit={onSubmit}
+              validationSchema={validationSchema}
+            >
+              {({ values, errors, handleChange, handleSubmit }) => (
+                <form onSubmit={handleSubmit} className="space-y-6">
+                  <Input
+                    label="Nombre"
+                    type="text"
+                    name="name"
+                    placeholder="Ingrese su nomre"
+                    error={errors.name}
+                    onChange={handleChange}
+                    value={values.name}
+                  />
+                  <Input
+                    label="Correo"
+                    type="email"
+                    name="email"
+                    placeholder="example@gmail.com"
+                    error={errors.email}
+                    onChange={handleChange}
+                    value={values.email}
+                  />
+                  <Input
+                    label="Contraseña"
+                    type="password"
+                    name="password"
+                    placeholder="**************"
+                    error={errors.password}
+                    onChange={handleChange}
+                    value={values.password}
+                  />
+                  <Input
+                    label="Confirmar contraseña"
+                    type="password"
+                    name="password_confirmation"
+                    placeholder="**************"
+                    error={errors.password_confirmation}
+                    onChange={handleChange}
+                    value={values.password_confirmation}
+                  />
 
-                    <Button type="submit" value="Registrar" />
-                  </form>
-                )}
-              </Formik>
-
-        
-        </div>
+                  <Button type="submit" value="Registrar" />
+                </form>
+              )}
+            </Formik>
+          </div>
         </div>
       </div>
     </section>
