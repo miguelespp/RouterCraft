7e413fb9bc2b43e33be362c7d9feac945bcd38e4
Owochan2004 <jhonridoutt@gmail.com>
2024-07-09 22:59:01 -0500

    jwt/force login update
    pa que sirve esto en una expo xd?

--- Frontend-RouterCraft/src/pages/layouts/BaseDashBoard.tsx
+++ Frontend-RouterCraft/src/pages/layouts/BaseDashBoard.tsx
@@ -1,24 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "./NavBar";
 import Header from "./Header";
 import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import {jwtDecode} from "jwt-decode"; // Correct import for jwt-decode
+import { ApiInstance } from '../../Services/Api'; // Ensure ApiInstance is imported
 
 const BaseDashBoard = () => {
   const [selectedOption, setSelectedOption] = useState('Dashboard');
-  // const { expanded, setExpanded } = stateNavBar();
   const [expanded, setExpanded] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+
+    if (!token) {
+      navigate("/login");
+      return;
+    }
+
+    try {
+      const decodedToken: { exp: number } = jwtDecode(token); // Use jwtDecode
+      const currentTime = Date.now() / 1000;
+
+      if (decodedToken.exp < currentTime) {
+        localStorage.removeItem("jwtToken");
+        navigate("/login");
+      } else {
+        ApiInstance.defaults.headers['Authorization'] = `Bearer ${token}`;
+      }
+    } catch (error) {
+      localStorage.removeItem("jwtToken");
+      navigate("/login");
+    }
+  }, [navigate]);
+
   return (
     <div className="container min-h-screen grid grid-cols-10">
-        <NavBar setSelectedOption={setSelectedOption} isExpanded={expanded} setExpandeState={setExpanded} />
+      <NavBar setSelectedOption={setSelectedOption} isExpanded={expanded} setExpandeState={setExpanded} />
       <div className={`${expanded ? 'col-span-8': 'col-span-9'} grid grid-cols-1`}>
         <header className="h-14 col-span-full">
           <Header selectedOption={selectedOption} />
         </header>
 
         <div className="col-span-3 flex-grow p-6 bg-gray-100 h-full w-full">
-          {/* <Mapa /> */}
-          {/* Outlet indica el children que se renderizara por la ruta */}
           <Outlet />
         </div>
         <footer className="col-span-full h-14">
