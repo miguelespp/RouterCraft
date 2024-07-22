7a0954f54f2d7d3b9f75fea734dffda8042298ac
miguelespp <carlos.espinoza24@unmsm.edu.pe>
2024-07-22 01:21:21 -0500

    Feat: add shadcn button and fix login interface

--- Frontend-RouterCraft/src/pages/Login.tsx
+++ Frontend-RouterCraft/src/pages/Login.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import * as Yup from "yup";
 import { ApiInstance } from "../Services/Api";
 import { Formik } from "formik";
 import Input from "../components/input/InputLabel";
-import Button from "../components/button/InputButton";
 import { useNavigate } from "react-router-dom";
+import { Button} from "@/components/ui/button.tsx";
+import { Loader2} from "lucide-react";
 
 const Login = () => {
+  const [submit, setSubmit] = useState<boolean>(false);
   const navigate = useNavigate();
   const initialValues = {
     email: "",
@@ -13,6 +16,7 @@ const Login = () => {
   };
 
   const onSubmit = async (values: typeof initialValues) => {
+    setSubmit(true);
     try {
       const response = await ApiInstance.post("/auth/login", values);
       console.log(response.data);
@@ -28,8 +32,10 @@ const Login = () => {
       if (error.response) {
         const response = error.response.data;
         console.log(response)
+        alert("Error al validar al usuario");
       }
     }
+    setSubmit(false);
   };
 
   const validationSchema = Yup.object({
@@ -42,15 +48,17 @@ const Login = () => {
   });
 
   return (
-    <section className="container bg-sky-600 h-full min-h-screen grid grid-cols-1 sm:grid-cols-2 content-center">
-      <div className="w-full h-full flex rounded">
-        <img className="items-center" src="src/assets/login.jpg" alt="login" />
+    <section className="bg-blue-500 h-full min-h-screen grid grid-cols-1 sm:grid-cols-2 content-center">
+      <div className="w-full h-full flex justify-center items-center bg-sky-300 rounded-r-3xl">
+        <div className="px-10 bg-amber-50">
+          <img className="shadow-2xl object-cover object-center rounded-full" src="src/assets/login.jpg" alt="login"/>
+        </div>
       </div>
       <div className="w-full mx-auto my-auto py-[15%] max-w-[60%]">
         <div className="grid grid-cols-1 gap-6 content-center h-full p-5 ">
           <div className="max-w-md w-full space-y-8 m-auto">
             <div className="py-2 my-4">
-              <h2 className="text-3xl font-bold text-center text-white">
+              <h2 className="text-4xl font-black text-center text-black hover:text-5xl">
                 LOGIN
               </h2>
             </div>
@@ -69,6 +77,7 @@ const Login = () => {
                     error={errors.email}
                     onChange={handleChange}
                     value={values.email}
+                    color={"black"}
                   />
                   <Input
                     label="Contraseña"
@@ -78,9 +87,20 @@ const Login = () => {
                     error={errors.password}
                     onChange={handleChange}
                     value={values.password}
+                    color={"black"}
                   />
 
-                  <Button type="submit" value="Login" />
+                  {!submit ? (
+                    <Button type="submit" value="Iniciar Sesion" >
+                        Iniciar Sesion
+                    </Button>
+                  ) : (
+                    <Button disabled={true} variant="secondary">
+                      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                      Please wait
+                    </Button>
+                      ) }
+
                 </form>
               )}
             </Formik>
