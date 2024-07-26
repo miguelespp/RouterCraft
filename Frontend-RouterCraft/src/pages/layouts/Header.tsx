befa81ae45d42b1995e2ab2848005e195144c782
miguelespp <carlos.espinoza24@unmsm.edu.pe>
2024-07-25 21:15:27 -0500

    Fix (header) : fix imports of shadcn components

--- Frontend-RouterCraft/src/pages/layouts/Header.tsx
+++ Frontend-RouterCraft/src/pages/layouts/Header.tsx
@@ -1,20 +1,64 @@
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar.tsx";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar.tsx";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu.tsx";
+import {Switch} from "@/components/ui/switch.tsx";
+import {ApiInstance} from "@/Services/Api.ts";
+import {Link} from "react-router-dom";
+import {IoIosLogOut, IoMdCog, IoMdContact, IoMdNotificationsOutline} from "react-icons/io";
 
 interface HeaderProps {
     selectedOption: string;
-  }
+}
 
-const Header = ({ selectedOption }:HeaderProps) => {
+
+const Header = ({selectedOption}: HeaderProps) => {
+    const handleLogout = () => {
+        const response = ApiInstance.post('/logout');
+        console.log(response);
+        localStorage.removeItem('token');
+        alert('Sesión cerrada');
+    };
+
     return (
-      <header className="h-full bg-gray-700 text-white p-2 flex justify-center content-center">
-        <h1 className={"flex-grow p-2"}>{selectedOption}</h1>
-          <Avatar className={"p-0"}>
-            <AvatarImage src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="Avatar" />
-              <AvatarFallback>CN</AvatarFallback>
-
-          </Avatar>
-      </header>
+        <header className="h-full bg-gray-700 text-white p-2 flex justify-between items-center relative">
+            <h1 className="p-1 text-2xl font-sans font-semibold absolute left-1/2 transform -translate-x-1/2">{selectedOption}</h1>
+            <div className="flex justify-end ml-auto">
+                <div className={"mr-5 my-auto p-1 border rounded-full"}>
+                    <IoMdNotificationsOutline className={"w-6 h-6"} />
+                </div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger className="pr-2">
+                        <Avatar>
+                            <AvatarImage
+                                src="https://lh3.googleusercontent.com/a/ACg8ocLx-3WB24PHBKweQeUcuigjE8BAcyQDTlIspkZP5sUXC1eqM6D_=s360-c-no"
+                                alt="Avatar"/>
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="mr-4 w-40">
+                        <DropdownMenuLabel className="flex items-center"><IoMdContact className="w-6"/> My
+                            account</DropdownMenuLabel>
+                        <DropdownMenuSeparator/>
+                        <DropdownMenuLabel className="flex items-center justify-between">DarkMode <Switch/>
+                        </DropdownMenuLabel>
+                        <DropdownMenuSeparator/>
+                        <DropdownMenuLabel className="flex items-center justify-start">
+                            <IoMdCog className="w-6"/> Settings
+                        </DropdownMenuLabel>
+                        <DropdownMenuLabel><Link to="/" className="cursor-pointer flex items-center"
+                                                 onClick={handleLogout}>
+                            <IoIosLogOut className="w-6"/> Logout
+                        </Link></DropdownMenuLabel>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </header>
     );
-  };
-  
-  export default Header;
+};
+
+export default Header;
--- Frontend-RouterCraft/src/pages/layouts/NavBar.tsx
+++ Frontend-RouterCraft/src/pages/layouts/NavBar.tsx
@@ -1,42 +1,34 @@
-import { Link  } from "react-router-dom";
-import { ApiInstance } from "../../Services/Api";
+import {Link} from "react-router-dom";
 
 export interface NavBarProps {
-  setSelectedOption: (option: string) => void;
-  setExpandeState  : (option: boolean) => void;
-  isExpanded?: boolean;
+    setSelectedOption: (option: string) => void;
+    setExpandeState: (option: boolean) => void;
+    isExpanded?: boolean;
 }
 
-const NavBar = ({ setSelectedOption, setExpandeState, isExpanded }:NavBarProps) => {
-  const handleLogout = () => {
-    const response = ApiInstance.post('/logout');
-    console.log(response);
-    localStorage.removeItem('token');
-    alert('Sesión cerrada');
-  };
+const NavBar = ({setSelectedOption, setExpandeState, isExpanded}: NavBarProps) => {
 
     return (
-      <div className={`bg-gray-800 text-white ${isExpanded ? 'col-span-2' : 'col-span-1'} h-full transition-all duration-300`}>
-        <button onClick={() => setExpandeState(!isExpanded)} className="p-2">
-          {isExpanded ? '<' : '>'}
-        </button>
-        <nav className="h-[80%]">
-          <ul className="space-y-3 h-full">
-            <li className="p-2 cursor-pointer">
-              <Link to="/dashboard" onClick={() => setSelectedOption('Home')}>Home</Link>
-            </li>
-            <li className="p-2 cursor-pointer">
-              <Link to="/dashboard/vehicle" onClick={() => setSelectedOption('Vehicles')}>Vehiculos</Link>
-            </li>
-            <li className="p-2 cursor-pointer">
-              <Link to="/dashboard/routes" onClick={() => setSelectedOption('Routes')}>Routes</Link>
-            </li>
-            
-          </ul>
-          <Link to="/" className="p-2 cursor-pointer" onClick={handleLogout}>Logout</Link>
-        </nav>
-      </div>
+        <div
+            className={`bg-gray-800 text-white ${isExpanded ? 'col-span-2' : 'col-span-1'} h-full transition-all duration-300`}>
+            <button onClick={() => setExpandeState(!isExpanded)} className="p-2">
+                {isExpanded ? '<' : '>'}
+            </button>
+            <nav className="h-auto">
+                <ul className="space-y-3 h-full mt-4">
+                    <li className="p-2 cursor-pointer">
+                        <Link to="/dashboard" onClick={() => setSelectedOption('Home')}>Home</Link>
+                    </li>
+                    <li className="p-2 cursor-pointer">
+                        <Link to="/dashboard/vehicle" onClick={() => setSelectedOption('Vehicles')}>Vehiculos</Link>
+                    </li>
+                    <li className="p-2 cursor-pointer">
+                        <Link to="/dashboard/routes" onClick={() => setSelectedOption('Routes')}>Routes</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
-  };
-  
+};
+
 export default NavBar;
