150a974ab406767d2ddb920f2f290d258fc9f3ef
miguelespp <miguelespinoza.dev.161@gmail.com>
2024-08-10 15:46:59 -0500

    Fix (navBar): fix style in buttons

--- Frontend-RouterCraft/src/pages/layouts/NavBar.tsx
+++ Frontend-RouterCraft/src/pages/layouts/NavBar.tsx
@@ -14,30 +14,38 @@ const NavBar = ({setSelectedOption, setExpandeState, isExpanded}: NavBarProps) =
 
     return (
         <nav
-            className={`flex-none bg-gray-700 text-white ${isExpanded ? 'w-36' : 'w-24'} transition-all duration-300 min-h-screen border`}>
+            className={`flex-none bg-gray-700 text-white ${isExpanded ? 'w-48' : 'w-28'} transition-all duration-300 min-h-screen border flex flex-col`}>
             <h2 className={"my-4 text-2xl font-bold"}>{isExpanded ? 'RouterCraft' : 'RC'}</h2>
-            <Separator/>
-            <button onClick={() => setExpandeState(!isExpanded)} className="p-2">
-                {isExpanded ? '<-|' : '|->'}
-            </button>
-            <div className="h-auto">
-                <ul className="space-y-3 h-full mt-4">
-                    <li className="p-2 cursor-pointer mx-6 border rounded bg-gray-500">
-                        <Link to="/dashboard" onClick={() => setSelectedOption('Home')}>
-                            {isExpanded ? 'Home' : <IoHome className={"mx-auto size-5"}/>}
-                        </Link>
-                    </li>
-                    <li className="p-2 cursor-pointer mx-6 border rounded bg-gray-500">
-                        <Link to="/dashboard/vehicle" onClick={() => setSelectedOption('Vehicles')}>
-                            {isExpanded ? 'Vehicles' : <FaTruck className={"mx-auto size-5"}/>}
-                        </Link>
-                    </li>
-                    <li className="p-2 cursor-pointer mx-6 border rounded bg-gray-500">
-                        <Link to="/dashboard/routes" onClick={() => setSelectedOption('Routes')}>
-                            {isExpanded ? 'Routes' : <TbRouteSquare className={"mx-auto size-5"}/>}
-                        </Link>
-                    </li>
-                </ul>
+            <Separator className="" />
+            <div className="flex flex-col justify-between h-full">
+                <div className="h-auto">
+                    <ul className=" cursor-pointer space-y-3 h-auto mt-4">
+                        <li className="mx-6 border rounded bg-gray-500 h-11">
+                            <Link className="flex justify-around px-3 size-full py-2" to="/dashboard"
+                                  onClick={() => setSelectedOption('Home')}>
+                                <IoHome className={"size-5"}/>{isExpanded ? <span>Home</span> : ''}
+                            </Link>
+                        </li>
+                        <li className="cursor-pointer mx-6 border rounded bg-gray-500 h-11">
+                            <Link className="flex justify-around px-3 py-2" to="/dashboard/vehicle"
+                                  onClick={() => setSelectedOption('Vehicles')}>
+                                <FaTruck className={"size-5"}/>{isExpanded ? <span>Vehicles</span> : ''}
+                            </Link>
+                        </li>
+                        <li className="cursor-pointer mx-6 border rounded bg-gray-500 h-11">
+                            <Link className="flex justify-around px-3 py-2" to="/dashboard/routes"
+                                  onClick={() => setSelectedOption('Routes')}>
+                                <TbRouteSquare className={"size-5"}/>{isExpanded ?
+                                <span>Routes</span> : ''}
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pb-8">
+                    <button onClick={() => setExpandeState(!isExpanded)} className="p-2 border rounded-full bg-white text-black ">
+                        {isExpanded ? '<-|' : '|->'}
+                    </button>
+                </div>
             </div>
         </nav>
     );
